be2d6a60b002d458aba46b6822d63c059241916f
Alireza Askari <alireza2023asgari@gmail.com>
2024-06-08 13:16:58 +0330

    Make file and folder structure.

---
+++ src/components/feature/Feature.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Feature = () => {
+  return <h1>feature</h1>;
+};
+
+export default Feature;
---
+++ src/containers/possibility/Possibility.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Possibility = () => {
+  return <h1>Possibility</h1>;
+};
+
+export default Possibility;
